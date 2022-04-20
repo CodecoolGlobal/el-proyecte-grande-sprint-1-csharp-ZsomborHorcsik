0edfd68299cdeec227c28b5db1498d6e532fb09b
rooland98 <rooland98@gmail.com>
2022-04-20 10:59:20 +0200

    add startup project , configured services

---
+++ FilmStock/Program.cs
@@ -0,0 +1,11 @@
+namespace FilmStock
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var builder = WebApplication.CreateBuilder();
+            Startup startup = new Startup(builder);
+        }
+    }
+}
