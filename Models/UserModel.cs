63ccb5a57aca7e8f4a0e2e39feec9f76f3c0f6dd
Zsombor Hörcsik <horcsik.zsombor88@gmail.com>
2022-05-03 12:43:28 +0200

    add UserModel

---
+++ Models/UserModel.cs
@@ -0,0 +1,25 @@
+﻿namespace FilmStock.Models
+{
+    public class UserModel
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public List<Review> Reviews { get; set; }
+
+        public List<Comment> Comments { get; set; }
+
+        public UserLevel Level { get; set; }
+    }
+
+    public enum UserLevel {
+        user,
+        premium,
+        admin
+    }
+
+}
+
