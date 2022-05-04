d199d9c4f2a1971101e5b36f9db23a919fa80bef
rooland98 <rooland98@gmail.com>
2022-05-04 20:36:29 +0200

    footer text color changed to black

--- filmstock/src/components/Footer.js
+++ filmstock/src/components/Footer.js
@@ -1,9 +1,9 @@
 const Footer = () =>{
     return ( 
         <footer class="md:flex md:items-center md:justify-between sm:px-12 px-4 bg-white py-7 text-black">
-            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="#test" class="hover:underline">FilmStock™</a>. All Rights Reserved.
+            <span class="text-sm text-black sm:text-center">© 2022 <a href="#test" class="hover:underline">FilmStock™</a>. All Rights Reserved.
             </span>
-            <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+            <ul class="flex flex-wrap items-center mt-3 text-sm text-black sm:mt-0">
                 <li>
                     <a href="#test" class="mr-4 hover:underline md:mr-6 ">About</a>
                 </li>
