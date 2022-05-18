0be91dfb8f2b40160266beddf3ac250f8c0fdb3c
Zsombor Hörcsik <horcsik.zsombor88@gmail.com>
2022-05-18 10:03:14 +0200

    add basic functions to User repository layer

--- Models/Interfaces/IUserRepository.cs
+++ Models/Interfaces/IUserRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace FilmStock.Models.Interfaces
+﻿using FilmStock.Models.Entities;
+
+namespace FilmStock.Models.Interfaces
 {
     public interface IUserRepository
     {
+        public Task Add(User user);
+        public Task Update(User user);
+        public Task Remove(long id);
+        public Task<User?> GetUser(long id);
     }
 }
--- Models/Repositories/UserRepository.cs
+++ Models/Repositories/UserRepository.cs
@@ -1,6 +1,40 @@
-﻿namespace FilmStock.Models.Repositories
+﻿using FilmStock.Models.Entities;
+using FilmStock.Models.Enums;
+using FilmStock.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmStock.Models.Repositories
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
+        private FilmContext _db;
+
+        public UserRepository(FilmContext db)
+        {
+            _db = db;
+        }
+
+        public async Task Add(User user)
+        {
+            _db.Users.Add(user);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<User?> GetUser(long id)
+        {
+            return await _db.Users.FirstOrDefaultAsync(user => user.Id == id);
+        }
+
+        public async Task Remove(long id)
+        {
+            _db.Users.Remove(_db.Users.Where(user => user.Id == id).First());
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task Update(User user)
+        {
+            _db.Users.Update(user);
+            await _db.SaveChangesAsync();
+        }
     }
 }
