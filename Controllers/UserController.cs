6f29bb642d2e31736168ae301d7541b32bfea587
Zsombor Hörcsik <horcsik.zsombor88@gmail.com>
2022-05-18 10:09:35 +0200

    add User Controller with proper routes to basic functions

---
+++ Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FilmStock.Models.Interfaces;
+using FilmStock.Models.Entities;
+
+namespace FilmStock.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _iuserRepository;
+
+        public UserController(IUserRepository iuserRepository)
+        {
+            _iuserRepository = iuserRepository;
+        }
+
+        [HttpPost("new")]
+        public async Task<IActionResult> AddUser([FromBody] User user)
+        {
+            await _iuserRepository.Add(user);
+            return CreatedAtAction("User created", new { user.Id }, user);
+
+        }
+
+        [HttpPut("edit/{Id}")]
+        public void EditUser(long Id, [FromBody] User user)
+        {
+            user.Id = Id;
+            _iuserRepository.Update(user);
+        }
+
+        [HttpDelete("delete/{Id}")]
+        public async Task DeleteUser(long Id)
+        {
+            await _iuserRepository.Remove(Id);
+        }
+
+        [HttpGet("{id:long}")]
+        public async Task<User?> GetUserById(long id)
+        {
+            return await _iuserRepository.GetUser(id);
+        }
+    }
+}
