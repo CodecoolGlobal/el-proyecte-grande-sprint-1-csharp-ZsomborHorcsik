bfe745f224629abebc72f7b8f76134d0d9cfb796
rooland98 <rooland98@gmail.com>
2022-05-19 16:38:38 +0200

    change footer background

--- filmstock/src/components/Footer.js
+++ filmstock/src/components/Footer.js
@@ -1,10 +1,10 @@
 import '../App.css';
 const Footer = () =>{
     return ( 
-        <footer id="footer" className="flex items-center justify-between px-12 px-4 bg-gray-500 py-7 text-black fix-footer">
-            <span className="text-sm text-black sm:text-center">© 2022 <a href="#test" className="hover:underline">FilmStock™</a>. All Rights Reserved.
+        <footer id="footer" className="flex items-center justify-between px-12 px-4 py-7 text-white fix-footer">
+            <span className="text-sm text-white sm:text-center">© 2022 <a href="#test" className="hover:underline">FilmStock™</a>. All Rights Reserved.
             </span>
-            <ul className="flex flex-wrap items-center mt-3 text-sm text-black sm:mt-0">
+            <ul className="flex flex-wrap items-center mt-3 text-sm text-white sm:mt-0">
                 <li>
                     <a href="#test" className="mr-4 hover:underline md:mr-6 ">About</a>
                 </li>
