44870c38374696405e6fb57e3b3433d409e78124
rooland98 <rooland98@gmail.com>
2022-06-01 12:22:42 +0100

    add header authorization & auth service methods

--- filmstock/src/_services/auth.service.js
+++ filmstock/src/_services/auth.service.js
@@ -6,7 +6,20 @@ function postFetch(urlFilter, payload){
                 .then((response)=>{
                     localStorage.setItem("user", JSON.stringify(response.data));
                 })
-
 }
 
-export default postFetch;
+const logout = () =>{
+    localStorage.removeItem("user");
+}
+
+const getCurrentUser = () =>{
+    return JSON.parse(localStorage.getItem("user"));
+}
+
+const authService = {
+    postFetch,
+    logout,
+    getCurrentUser
+}
+
+export default authService;
