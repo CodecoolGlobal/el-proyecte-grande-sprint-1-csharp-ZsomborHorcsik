c30c0c3132a036cc5715ca29968f2dd79558e829
Zsombor Hörcsik <horcsik.zsombor88@gmail.com>
2022-06-01 15:41:35 +0200

    add CollectionDisplay

---
+++ filmstock/src/components/CollectionDisplay.js
@@ -0,0 +1,24 @@
+import '../App.css';
+import "@material-tailwind/react/tailwind.css";
+import FilmCard from '../components/FilmCard';
+import useFetch from '../Hooks/useFetch';
+import Layout from './Layout';
+
+const CollectionDisplay = (props) => {
+    const { id } = useParams();
+    const {data, error} = useFetch(`api/user/collection/${id}`);
+
+    if(error) console.log(error);
+
+  return (
+    <div className="CollectionDisplay">
+        <Layout>
+            <div className={`grid grid-cols-6 gap-4 md:container md:mx-auto mt-6`}>
+                {data?.map(movieData =>(<FilmCard movie={movieData} key={movieData.id}/>))}
+            </div>
+        </Layout>
+    </div>
+  );
+}
+
+export default CollectionDisplay;
--- filmstock/src/components/UserDisplay.js
+++ filmstock/src/components/UserDisplay.js
@@ -12,22 +12,22 @@ const UserDisplay = (props) => {
           <Layout>
             <div class="center">
                 <div class="container">
-                <div class="left">  
-                    <div class="title__contain">
-                    <div class="username">{data?.userName}</div>
-                    <div class="title">Proffessional Critic</div>
+                    <div class="left">  
+                        <div class="title__contain">
+                        <div class="username">{data?.userName}</div>
+                        <div class="title">Proffessional Critic</div>
+                        </div>
                     </div>
-                </div>
-                <div class="right">
-                    <div class="rightbox">
-                    <span class="large">{data?.Collection.length}</span>
-                    <span class="small">Posts</span>
+                    <div class="right">
+                        <div class="rightbox">
+                        <span class="large">{data?.Collection.length}</span>
+                        <span class="small">Movies in collection</span>
+                        </div>
+                        <div class="rightbox">
+                        <span class="small">Email:</span>
+                        <span class="small">{data?.email}</span>
+                        </div>
                     </div>
-                    <div class="rightbox">
-                    <span class="small">Email:</span>
-                    <span class="small">{data?.email}</span>
-                    </div>
-                </div>
                 </div>
             </div>
           </Layout>
